9a6085a0bacaa2f252c464f0c9e4999f811741fd
rep4975 <rep4975@IGM2350-21>
2018-03-22 19:04:24 -0400

    Perlin Noise Progress

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 import {perlinNoise} from './perlinNoise.js';
 export {mapInitialization, drawMap};
 
-const seed = generateSeed();
+const seed = 5.393906695411715;// generateSeed();
 const tileSize = 25;
 
 function generateSeed(){
@@ -10,19 +10,26 @@ function generateSeed(){
 
 function mapInitialization(startX = 0, startY = 0, rows = 100, columns = 100){
     let map = [];
-    console.log("UPDATED");
+    rows = Math.round(rows);
+    columns = Math.round(columns);
+
+    console.log("startX: " + startX + " StartY: " + startY + " Rows: " + rows + " Columns " + columns + " Seed: " + seed);
+    
     for(let i = startX; i < rows; i++){
         map[i] = [];
         for(let j = startY; j < columns; j++){
-            let x = (i - 1 * seed % 256 + i * seed % 256 + i + 1 * seed % 256) / 3;
-            let y = (j - 1 * seed % 256 + j * seed % 256 + j + 1 * seed % 256) / 3;
-            let noise = perlinNoise(x, y, 0);
+            // let x = (i - 1 * seed % 256 + i * seed % 256 + i + 1 * seed % 256) / 3;
+            // let y = (j - 1 * seed % 256 + j * seed % 256 + j + 1 * seed % 256) / 3;
+            let n = (i * 2 * j * 3) * seed % 256;
+            // console.log("n: " + n);
+            let noise = Math.abs(perlinNoise(n));
+            // let noise = perlinNoise(x, y, 0);
             // let noise = perlinNoise(i * seed % 256, j * seed % 256, 0);//getSeededPosition(i, j);
             // console.log("Noise: " + noise);
-            if(noise < 0.1){
+            if(noise < 0.05){
                 map[i][j] = 0;
             }
-            else if(noise < 0.4){
+            else if(noise < 0.95){
                 map[i][j] = 1;
             }
             else{
@@ -53,13 +60,13 @@ function drawMap(ctx, map){
         for(let y = 0; y < map[x].length; y++){
             switch(map[x][y]){
                 case 0:
-                    color = 'rgba(112, 128, 144, 1)'; // Stone
+                    color = 'rgba(0, 123, 153, 1)'; // Water
                     break;
                 case 1:
-                    color = 'rgba(87, 59, 12, 1)'; // Dirt
+                    color = 'rgba(11, 102, 35, 1)'; // Grass
                     break;
                 case 2:
-                    color = 'rgba(11, 102, 35, 1)'; // Grass
+                    color = 'rgba(112, 128, 144, 1)'; // Stone
                     break;
             }
             ctx.fillStyle = color;
